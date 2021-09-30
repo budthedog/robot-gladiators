3bc63447b0053956f9f4dc0e55d7f186b6d00aba
mitchell <mitchellmurray48@gmail.com>
2021-09-29 19:07:10 -0500

    round-one

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,6 +31,17 @@ var fight = function() {
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
-};
+      // put new code under this
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+        // check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+  };
+
 
 fight();
